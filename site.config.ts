f7d2a686c2b9d7e0dafbed53282db05c2c457754
MURENL <110599920+MURENL@users.noreply.github.com>
2023-05-26 17:06:58 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId:Muren-s-Blog,
+  rootNotionSpaceId: Muren-s-Blog,
 
   // basic site info (required)
   name: 'Muren Blog',
